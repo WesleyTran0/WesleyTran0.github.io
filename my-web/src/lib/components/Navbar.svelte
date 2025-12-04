961529adbf43587af109aa011ce3e8a68f1464b8
WesleyTran0 <wesley.tran24@gmail.com>
2025-12-04 10:50:44 -0500

    initial zommit

---
+++ my-web/src/lib/components/Navbar.svelte
@@ -0,0 +1,8 @@
+<div class="w-full h-14">
+	<nav class="flex flex-row justify-center items-center px-6">
+		<div class="flex gap-2">
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+		</div>
+	</nav>
+</div>
