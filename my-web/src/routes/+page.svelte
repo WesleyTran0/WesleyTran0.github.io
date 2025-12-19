35d2adadac162a860f19111bc42dc8b1b91e794b
WesleyTran0 <wesley.tran24@gmail.com>
2025-12-19 10:36:54 -0500

    added marked dependency and fixed spelling errors on tailwind styling

--- my-web/src/routes/+page.svelte
+++ my-web/src/routes/+page.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
 	import Card from '$lib/components/Card.svelte';
-	import Navbar from '$lib/components/Navbar.svelte';
 	import Project from '$lib/components/Project.svelte';
 </script>
 
-<div class="flex flex-cols w-full gap-4">
+<div class="flex flex-row w-full gap-4">
 	<div class="flex items-center">
 		<img src={'temp'} alt={'image of me failed to load'} loading="lazy" />
 	</div>
 	<Card class="">
-		<header class="flex flex-row text-2xl text-accent font-bold pb-4">
+		<header class="flex flex-row pb-4">
 			Hi, I'm Wesley <div
 				class="font-normal px-2 bg-linear-to-r from-blue-400 via-green-600 to-yellow-400 gradient-animation bg-clip-text text-transparent bg-size-[200%_auto]"
 			>
@@ -26,5 +25,5 @@
 	</Card>
 </div>
 <div class="grid grid-rows-2 gap-4">
-	<Project title="test_featured" heading="Latest Work" />
+	<Project title="test_featured" heading="Latest Work" size="small" image="fuck" />
 </div>
--- my-web/src/routes/projects/+page.svelte
+++ my-web/src/routes/projects/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import Navbar from '$lib/components/Navbar.svelte';
 	import Project from '$lib/components/Project.svelte';
 </script>
 
-<div class="flex flex-rows">
-	<Project title="test_featured" />
+<div class="flex flex-row">
+	<Project title="test_featured" size="large" image="FUCK" />
 </div>
